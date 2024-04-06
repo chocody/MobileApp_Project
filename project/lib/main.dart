6a7499b493e0eb69458947bf9887f7e74e6a3b8b
chocody <108443531+chocody@users.noreply.github.com>
2024-04-06 17:14:10 +0700

    update event page
    can click banner using gesturedectector

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -74,97 +74,102 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Padding banner_event(join) {
+  GestureDetector banner_event(join) {
     Color status_color =
         join ? Color.fromRGBO(123, 229, 7, 1) : Color.fromRGBO(255, 69, 84, 1);
-    IconData status_icon = join ? Icons.check_circle_outline : Icons.cancel_outlined;
+    IconData status_icon =
+        join ? Icons.check_circle_outline : Icons.cancel_outlined;
 
-    return Padding(
-      padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(25.0),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25.0),
-            image: const DecorationImage(
-              image: AssetImage('assets/picture/court1.png'),
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => {},
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              image: const DecorationImage(
+                image: AssetImage('assets/picture/court1.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: MaterialBanner(
-            padding: EdgeInsets.all(20.0), // Inner padding
-            content: Stack(
-              children: <Widget>[
-                // Stroked text as border.
-                Text(
-                  'Basketball Court 1',
-                  style: TextStyle(
-                    fontSize: 20,
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 3
-                      ..color = Colors.black,
+            child: MaterialBanner(
+              padding: EdgeInsets.all(20.0), // Inner padding
+              content: Stack(
+                children: <Widget>[
+                  // Stroked text as border.
+                  Text(
+                    'Basketball Court 1',
+                    style: TextStyle(
+                      fontSize: 20,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 3
+                        ..color = Colors.black,
+                    ),
                   ),
-                ),
-                // Solid text as fill.
-                Text(
-                  'Basketball Court 1',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+                  // Solid text as fill.
+                  Text(
+                    'Basketball Court 1',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            backgroundColor: Colors.transparent, // Make background transparent
-            actions: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Stack(
-                    children: <Widget>[
-                      // Stroked text as border.
-                      Text(
-                        '13:00-15:00',
-                        style: TextStyle(
-                          fontSize: 30,
-                          foreground: Paint()
-                            ..style = PaintingStyle.stroke
-                            ..strokeWidth = 3
-                            ..color = Colors.black,
-                        ),
-                      ),
-                      // Solid text as fill.
-                      Text(
-                        '13:00-15:00',
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  )
                 ],
               ),
-              Stack(children: [
-                Container(
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: status_color,
+              backgroundColor:
+                  Colors.transparent, // Make background transparent
+              actions: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: <Widget>[
+                        // Stroked text as border.
+                        Text(
+                          '13:00-15:00',
+                          style: TextStyle(
+                            fontSize: 30,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 3
+                              ..color = Colors.black,
+                          ),
+                        ),
+                        // Solid text as fill.
+                        Text(
+                          '13:00-15:00',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Stack(children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: status_color,
+                    ),
                   ),
-                ),
-                Icon(
-                  status_icon,
-                  color: Colors.white,
-                  size: 30,
-                ),
-              ]),
-              SizedBox(
-                height: 10,
-              )
-            ],
+                  Icon(
+                    status_icon,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ]),
+                SizedBox(
+                  height: 10,
+                )
+              ],
+            ),
           ),
         ),
       ),
