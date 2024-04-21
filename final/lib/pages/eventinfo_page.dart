e75469eaf66235f62afa78252ad1531b0d1ac919
chocody <108443531+chocody@users.noreply.github.com>
2024-04-21 20:50:41 +0700

    done

--- final/lib/pages/eventinfo_page.dart
+++ final/lib/pages/eventinfo_page.dart
@@ -28,7 +28,6 @@ class _EventInfoPageState extends State<EventInfoPage> {
       backgroundColor: Color.fromRGBO(244, 230, 217, 1),
     );
   }
-
   Widget _buildEventInfo(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
       future: _firestore.collection("events").doc(widget.eid).get(),
@@ -41,133 +40,280 @@ class _EventInfoPageState extends State<EventInfoPage> {
           return Text('No data available');
         } else {
           final data = snapshot.data!.data()! as Map<String, dynamic>;
-
+          String gid = data["gid"];
           return FutureBuilder<DocumentSnapshot>(
-              future: _firestore.collection("groups").doc(data["gid"]).get(),
-              builder: (context, groupSnapshot) {
-                final Gdata = snapshot.data!.data()! as Map<String, dynamic>;
-                print(Gdata);
-                return Column(
-                  children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                      width: 300.0,
-                      height: 180.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        image: DecorationImage(
-                          image: NetworkImage(data["image"]),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
+              future: _firestore.collection("groups").doc(gid).get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error fetching data');
+                } else if (!snapshot.hasData || snapshot.data == null) {
+                  return Text('No data available');
+                } else {
+                  final gdata = snapshot.data!.data()! as Map<String, dynamic>;
+                  print(gdata);
+                  return Column(
                       children: [
                         SizedBox(
-                          width: 30,
+                          height: 30,
                         ),
-                        text(data["eventName"], Colors.black, 24)
-                      ],
-                    ),
-                    SizedBox(
-                      height: 1,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 30,
-                        ),
-                        text(Gdata["groupName"], Colors.black, 16)
-                      ],
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 25,
-                        ),
-                        Icon(
-                          Icons.access_time,
-                          size: 30,
+                        Container(
+                          width: 300.0,
+                          height: 180.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            image: DecorationImage(
+                              image: NetworkImage(data["image"]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                         SizedBox(
-                          width: 5,
+                          height: 10,
                         ),
-                        text("${data["date"]}, ${data["time"]}", Colors.black,
-                            16)
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: <Widget>[
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 30,
+                            ),
+                            text(data["eventName"], Colors.black, 24)
+                          ],
+                        ),
                         SizedBox(
-                          width: 30,
+                          height: 1,
                         ),
-                        text(data["description"], Colors.black, 16),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    SizedBox(
-                      width: 300.0, // Set the desired width
-                      child: ElevatedButton(
-                          onPressed: () {
-                            _eventService.joinedEvent(widget.eid);
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 30,
+                            ),
+                            text(gdata["groupName"], Colors.black, 16)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 25,
+                            ),
+                            Icon(
+                              Icons.access_time,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            text("${data["date"]}, ${data["time"]}",
+                                Colors.black, 16)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width: 30,
+                            ),
+                            text(data["description"], Colors.black, 16),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        SizedBox(
+                          width: 300.0, // Set the desired width
+                          child: ElevatedButton(
+                              onPressed: () {
+                                _eventService.joinedEvent(widget.eid);
                             setState(() {
                               widget.joined = !widget.joined;
                               // print(widget.joined);
                             });
-                          },
-                          child:
-                              Text(widget.joined ? "Leave event" : "Let's join",
+                              },
+                              child: Text(widget.joined ? "Leave event" : "Let's join",
                                   style: TextStyle(
                                     color: Colors.white,
                                     fontFamily: 'Poppins',
                                     fontSize: 16,
                                     fontWeight: FontWeight.bold,
                                   )),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromRGBO(164, 151, 134, 1),
-                          )),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 30,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromRGBO(164, 151, 134, 1),
+                              )),
                         ),
-                        text("Participation:", Colors.black, 16)
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 30,
+                            ),
+                            text("Participation:", Colors.black, 16)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+
+                        Expanded(child: _buildJoinedEventList(context)),
+
+                        SizedBox(
+                          height: 20,
+                        ),
                       ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-
-                    //list tile
-                    Expanded(child: _buildJoinedEventList(context)),
-
-                    SizedBox(
-                      height: 20,
-                    ),
-                  ],
-                );
+                    );
+                }
               });
         }
       },
     );
   }
+
+  // Widget _buildEventInfo(BuildContext context) {
+  //   return FutureBuilder<DocumentSnapshot>(
+  //     future: _firestore.collection("events").doc(widget.eid).get(),
+  //     builder: (context, snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         return CircularProgressIndicator();
+  //       } else if (snapshot.hasError) {
+  //         return Text('Error fetching data');
+  //       } else if (!snapshot.hasData || snapshot.data == null) {
+  //         return Text('No data available');
+  //       } else {
+  //         final data = snapshot.data!.data()! as Map<String, dynamic>;
+
+  //         return FutureBuilder<DocumentSnapshot>(
+  //             future: _firestore.collection("groups").doc(data["gid"]).get(),
+  //             builder: (context, groupSnapshot) {
+  //               final Gdata = snapshot.data!.data()! as Map<String, dynamic>;
+  //               print(Gdata);
+  //               return Column(
+  //                 children: [
+  //                   SizedBox(
+  //                     height: 30,
+  //                   ),
+  //                   Container(
+  //                     width: 300.0,
+  //                     height: 180.0,
+  //                     decoration: BoxDecoration(
+  //                       borderRadius: BorderRadius.circular(12.0),
+  //                       image: DecorationImage(
+  //                         image: NetworkImage(data["image"]),
+  //                         fit: BoxFit.cover,
+  //                       ),
+  //                     ),
+  //                   ),
+  //                   SizedBox(
+  //                     height: 10,
+  //                   ),
+  //                   Row(
+  //                     children: [
+  //                       SizedBox(
+  //                         width: 30,
+  //                       ),
+  //                       text(data["eventName"], Colors.black, 24)
+  //                     ],
+  //                   ),
+  //                   SizedBox(
+  //                     height: 1,
+  //                   ),
+  //                   Row(
+  //                     children: [
+  //                       SizedBox(
+  //                         width: 30,
+  //                       ),
+  //                       text(Gdata["groupName"], Colors.black, 16)
+  //                     ],
+  //                   ),
+  //                   SizedBox(
+  //                     height: 5,
+  //                   ),
+  //                   Row(
+  //                     children: [
+  //                       SizedBox(
+  //                         width: 25,
+  //                       ),
+  //                       Icon(
+  //                         Icons.access_time,
+  //                         size: 30,
+  //                       ),
+  //                       SizedBox(
+  //                         width: 5,
+  //                       ),
+  //                       text("${data["date"]}, ${data["time"]}", Colors.black,
+  //                           16)
+  //                     ],
+  //                   ),
+  //                   SizedBox(
+  //                     height: 10,
+  //                   ),
+  //                   Row(
+  //                     children: <Widget>[
+  //                       SizedBox(
+  //                         width: 30,
+  //                       ),
+  //                       text(data["description"], Colors.black, 16),
+  //                     ],
+  //                   ),
+  //                   SizedBox(
+  //                     height: 15,
+  //                   ),
+  //                   SizedBox(
+  //                     width: 300.0, // Set the desired width
+  //                     child: ElevatedButton(
+  //                         onPressed: () {
+  //                           _eventService.joinedEvent(widget.eid);
+  //                           setState(() {
+  //                             widget.joined = !widget.joined;
+  //                             // print(widget.joined);
+  //                           });
+  //                         },
+  //                         child:
+  //                             Text(widget.joined ? "Leave event" : "Let's join",
+  //                                 style: TextStyle(
+  //                                   color: Colors.white,
+  //                                   fontFamily: 'Poppins',
+  //                                   fontSize: 16,
+  //                                   fontWeight: FontWeight.bold,
+  //                                 )),
+  //                         style: ElevatedButton.styleFrom(
+  //                           backgroundColor: Color.fromRGBO(164, 151, 134, 1),
+  //                         )),
+  //                   ),
+  //                   SizedBox(
+  //                     height: 20,
+  //                   ),
+  //                   Row(
+  //                     children: [
+  //                       SizedBox(
+  //                         width: 30,
+  //                       ),
+  //                       text("Participation:", Colors.black, 16)
+  //                     ],
+  //                   ),
+  //                   SizedBox(
+  //                     height: 10,
+  //                   ),
+
+  //                   //list tile
+  //                   Expanded(child: _buildJoinedEventList(context)),
+
+  //                   SizedBox(
+  //                     height: 20,
+  //                   ),
+  //                 ],
+  //               );
+  //             });
+  //       }
+  //     },
+  //   );
+  // }
 
   // build user list
   Widget _buildJoinedEventList(BuildContext context) {
